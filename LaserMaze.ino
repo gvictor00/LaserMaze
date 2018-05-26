fe30b6581a4c2ffed5d90c003476c0eaa3e9a86c
George Victor <gvictor00@gmail.com>
2018-05-26 18:13:22 -0300

    Descreve algumas variáveis

--- LaserMaze.ino
+++ LaserMaze.ino
@@ -8,9 +8,34 @@
  * 0.1.0 - Inicio do desenvolvimento
  */
 
-void setup() {
-  // put your setup code here, to run once:
+// OUTPUT
+#define rele_laser   7
+#define rele_alarme  8
+#define rele_teclado 9
 
+// INPUT
+#define input_rfid_inicio 5
+#define input_rfid_final  6
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Inicializando pinos...");
+  
+  pinMode(rele_laser,   OUTPUT);    // Liga/Desliga os lasers
+  pinMode(rele_alarme,  OUTPUT);    // Liga/Desliga o alarme e a sirene
+  pinMode(rele_teclado, OUTPUT);    // Liga/Desliga o teclado de saída ou fornece ou não energia para a trava elétrica.
+
+  pinMode(input_rfid_inicio, INPUT);  // Recebe sinal do RFID de inicio
+  pinMode(input_rfid_final,  INPUT);  // Recebe sinal do RFID do final
+
+  digitalWrite(rele_laser,    LOW); // Inicia o relê desligado
+  digitalWrite(rele_alarme,   LOW); // Inicia o relê desligado
+  digitalWrite(rele_teclado,  LOW); // Inicia o relê desligado
+
+  Serial.println("Setup de pinos finalizado...");
+  
+  delay(2000);
+  Serial.println("Inicializando programa!");
 }
 
 void loop() {
