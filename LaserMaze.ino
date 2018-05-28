05ce172e7820259d0c4a8c30eecccecebfd5c326
George Victor <gvictor00@gmail.com>
2018-05-27 22:33:57 -0300

    Inclui loop de processamento

--- LaserMaze.ino
+++ LaserMaze.ino
@@ -1,12 +1,12 @@
 /*
- * @Project: Laser maze
- * @Author: George Victor
- * @Description: Labirinto baseado em lasers que tem como pontos de ativação e desativaçãso
- *              leitura de tags RFID. 
- * @Version:
- * 
- * 0.1.0 - Inicio do desenvolvimento
- */
+   @Project: Laser maze
+   @Author: George Victor
+   @Description: Labirinto baseado em lasers que tem como pontos de ativação e desativaçãso
+                leitura de tags RFID.
+   @Version:
+
+   0.1.0 - Inicio do desenvolvimento
+*/
 
 // OUTPUT
 #define rele_laser   7
@@ -22,16 +22,28 @@
 #define sensor2 A1
 #define sensor3 A2
 
+// Variáveis de leitura e auxiliares
 int leitura_sensor1 = 0;
 int leitura_sensor2 = 0;
 int leitura_sensor3 = 0;
 
+int leitura_rfid_inicio = 0;
+int leitura_rfid_final = 0;
+
+bool inicio = true;
+bool bateuSensor = false;
+
+// Limiar de luminosidade para considerar que "bateu" no laser.
+int limiarSensores = 300;
+
+// Leitura sensores - número de leituras para evitar ruido
+int leiturasSensor = 50
 int i = 0;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Inicializando pinos...");
-  
+
   pinMode(rele_laser,   OUTPUT);    // Liga/Desliga os lasers
   pinMode(rele_alarme,  OUTPUT);    // Liga/Desliga o alarme e a sirene
   pinMode(rele_teclado, OUTPUT);    // Liga/Desliga o teclado de saída ou fornece ou não energia para a trava elétrica.
@@ -50,7 +62,7 @@ void setup() {
 
   digitalWrite(rele_laser, HIGH);   // Liga os lasers
   delay(100);
-  for(i = 0; i < 50; i++)
+  for (i = 0; i < leiturasSensor; i++)
   {
     Serial.print(".");
     leitura_sensor1 += analogRead(sensor1);
@@ -59,10 +71,10 @@ void setup() {
     delay(100);
   }
 
-  leitura_sensor1 /= 50;
-  leitura_sensor2 /= 50;
-  leitura_sensor3 /= 50;
-  
+  leitura_sensor1 /= leiturasSensor;
+  leitura_sensor2 /= leiturasSensor;
+  leitura_sensor3 /= leiturasSensor;
+
   Serial.println();
 
   Serial.print("Sensor #1: ");
@@ -71,13 +83,55 @@ void setup() {
   Serial.println(leitura_sensor2);
   Serial.print("Sensor #3: ");
   Serial.println(leitura_sensor3);
-  
+
   Serial.println("======================");
   Serial.println("Sensores calibrados!!!");
   Serial.println("======================");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (inicio == true) // Lê o rfid do inicio
+  {
+    leitura_rfid_inicio = digitalRead(input_rfid_inicio);
+    if (leitura_rfid_inicio == 1)
+    {
+      inicio = false;
+
+      if (bateuSensor == true)
+      {
+        // Liga o rele dos lasers
+        digitalWrite(rele_laser, HIGH);
+
+        // Desliga a sirene/girflex
+        digitalWrite(rele_sirene, LOW);
+      }
+    }
+  }
+  else // Lê os sensores dos lasers
+  {
+    for (i = 0; i < leiturasSensor; i++) {
+      leitura_sensor1 = analogRead(sensor1);
+      leitura_sensor2 = analogRead(sensor2);
+      leitura_sensor3 = analogRead(sensor3);
+
+      leitura_sensor1 /= leiturasSensor;
+      leitura_sensor2 /= leiturasSensor;
+      leitura_sensor3 /= leiturasSensor;
+    }
+
+    if (leitura_sensor1 <= limiarSensor || leitura_sensor2 <= limiarSensor || leitura_sensor3 <= limiarSensor)
+    {
+      // Aciona flag de batida no sensor
+      bateuSensor = true;
+
+      // Desliga o rele dos lasers
+      digitalWrite(rele_laser, LOW);
+
+      // Liga a sirene/girflex
+      digitalWrite(rele_sirene, HIGH);
+    }
+  }
+
+
 
 }
